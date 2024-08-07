5828c5f6fd8a587dd784ded9f9874c49b75425e7
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-08-07 20:56:18 +0530

    day 3

--- day-3/Activity-2.js
+++ day-3/Activity-2.js
@@ -1,3 +1,18 @@
 /*  Nested if-Else statements.*/
 //Task 3:- Write a program to find the largest of three numbers using nested if-else statements.
 
+let a = 10;
+let b = 20;
+let c = 30;
+if (a>b) {
+    if (a>c) {
+       console.log(a); 
+    }
+    else{
+        if (b>c) {
+            console.log(b);
+            
+        }
+    }
+    
+}
