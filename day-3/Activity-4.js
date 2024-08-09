965a6e3b0bbf6593ba140def4bfd76dfa1329c15
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-08-09 17:57:31 +0530

    day 3

--- day-3/Activity-4.js
+++ day-3/Activity-4.js
@@ -1,3 +1,6 @@
-/* Conditional (Ternary) Operator
+/* Conditional (Ternary) Operator*/
 // Task 6 :- Write the program that uses the ternary operator to check if a number is even or odd and log the result to the console.
 
+const number= 34;
+const result = number % 2=== 0? "even":"odd"
+console.log(result);
