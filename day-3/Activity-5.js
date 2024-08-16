8999e99a807301b0e7ebb75463c277e9f686d1c6
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-08-16 21:31:50 +0530

    task

--- day-3/Activity-5.js
+++ day-3/Activity-5.js
@@ -1,3 +1,27 @@
 /* Combining Conditions*/
 
 // Task 7 :- Write a program to check if a year is a leap year using multiple condtions (divisible by 4, but not 100 unless also divisible by 400 and log the result to the console)
+
+let year = 2024;
+let result = year % 4 ===0?
+"leapyear":"notleapyear"
+console.log(result);
+
+// Feature Request:
+// 1. Number Check Script: Write a script that checks is a number is positive, negative, zero using if-else statements and logs the result.
+
+// 2. Voting Eligibility Script: Write a script that checks if a person is eligible to vote based on their age and log the result.
+
+// 3. Day of the Week Script: Write a script that uses a switch case to determine the day of the week based on a number (1-7) and log the day name.
+
+// 4. Grade Assignment Script: Write a script that uses a switch case to assign a grade ('A', 'B', 'C', 'D', 'F') based on a score and log the grade.
+
+// 5. Leap Year Check Script: Write a script that checks if a year is a leap year using multiple conditions and log the result.
+
+
+// Achievement:
+// Implement and understand basic if-else control flow.
+// Use nested if-else statements to handle multiple conditions.
+// Utilize switch cases for control flow based on specific values.
+// Apply the ternary operator for conditional checking.
+// Combine multiple conditions to solve more complex problems.
