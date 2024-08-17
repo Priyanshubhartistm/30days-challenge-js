ec6813ceea5f6d2b7233bc06464647e57903aa3c
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-08-17 15:49:32 +0530

    activity

--- day-4/Activity-1.js
+++ day-4/Activity-1.js
@@ -2,6 +2,16 @@
 
 // Task 1 :- Write a program to print numbers from 1 to 10 using a for loop.
 
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+    
+}
 
 
 // Task 2 :- Write a program to print the multiplication table of 5 using a for loop.
+let num=5
+for (let i = 1; i <= 10; i++) {
+    result = num*i
+    console.log(result);
+    
+}
