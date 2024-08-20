17b48c7cdd0db9ef3687b8061576c567f08eb41d
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-08-21 00:55:49 +0530

    activity

--- day-4/Activity-2.js
+++ day-4/Activity-2.js
@@ -1,2 +1,13 @@
 /* While loop */
-// Task 3:-
+// Task 3:-write a program to calculate the sum of numbers from 1 to 10 using a while loop.
+
+let sum = 0
+let num= 1
+while (num<=10) {
+    sum =sum+num
+    num++
+    
+}
+console.log(sum);
+
+// Task 4:- Write a program to print numbers from 10 to 1 using a while loop.
