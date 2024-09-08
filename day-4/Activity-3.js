22576a9286b7bb898abffad3b8462ea682f294a9
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-09-08 23:26:28 +0530

    activity

---
+++ day-4/Activity-3.js
@@ -0,0 +1,9 @@
+// Do -while loop
+// Task 5:- Write a program to print numbers from 1 to 5 using a do while loop.
+
+
+
+
+
+//  Task 6:- Write a program to calculate the factorial of a number using a do - while loop.
+ 
