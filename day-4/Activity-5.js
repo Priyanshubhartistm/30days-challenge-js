af8f0886c423b0b44abe8f29893917e78e575e93
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-03-12 23:29:48 +0530

    ac

---
+++ day-4/Activity-5.js
@@ -0,0 +1,11 @@
+// Loop control statements
+
+// Task 8:- Write a program to print numbers from 1 to 10 ,but skip the numbers 5 using the continue statement.
+
+
+
+
+
+
+
+// Task 9:- Write a program to print numbers from 1 to 10 ,but stop the loop, when the number is 7 using the the break statement.
